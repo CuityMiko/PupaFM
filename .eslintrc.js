ed25e39a598aeaafeae299a5d8e60a1bf6d3a965
xwartz <stddup@gmail.com>
2016-04-08 15:37:56 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,25 @@
 module.exports = {
     "extends": "standard",
+    "env": {
+      "browser": true,
+      "mocha": true,
+      "node": true
+    },
+    "rules": {
+      "react/jsx-uses-react": 2,
+      "react/jsx-uses-vars": 2,
+      "react/react-in-jsx-scope": 2,
+      "react/sort-comp": 0,
+      "react/no-multi-comp": 0,
+      "comma-dangle": 0,
+      "id-length": 0,
+      "new-cap": 0,
+      "eol-last": 0,
+      "jsx-quotes": 0,
+      "consistent-return": 0
+    },
     "plugins": [
         "standard",
         "react"
-    ],
-    "settings": {
-      "react": {
-      "pragma": "React",  // Pragma to use, default to "React"
-      "version": "0.14.7" // React version, default to the latest React stable release
-    }
-  },
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  }
+    ]
 };
