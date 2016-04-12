524814edafa4955aa55f51dd4857fdce72ecbe90
xwartz <stddup@gmail.com>
2016-04-12 09:55:21 +0800

    fix iconfont load

--- webpack.config.js
+++ webpack.config.js
@@ -26,14 +26,11 @@ module.exports = {
       include: [path.resolve(__dirname, 'app')],
       loader: 'style!css!sass'
     }, {
-      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-    }, {
-      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'file-loader'
+      test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      loader: 'url'
     }, {
       test: /\.(png|jpg)$/,
-      loader: 'url-loader'
+      loader: 'url'
     }, {
       test: /\.json$/,
       loader: 'json'
