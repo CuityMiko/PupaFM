56f5fa42a7fbc3b4964adf767b2c03f8b2f2fb32
xwartz <stddup@gmail.com>
2016-05-06 09:41:44 +0800

    更改svg显示

--- src/components/channel/item.js
+++ src/components/channel/item.js
@@ -11,13 +11,25 @@ class Channel extends Component {
     this.props.channelClick(id)
   }
 
+  renderIcon () {
+    return (
+      <svg size="12" version="1.1" color="#5CBC7D" className="icon" viewBox="0,0,12,12" height="12" width="12" >
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none">
+          <line className="icon-playing-bar1" stroke-width="1" stroke="#5CBC7D" stroke-linecap="round" id="line-1" x1="2" y1="12" x2="2" y2="1"></line>
+          <line className="icon-playing-bar2" stroke-width="1" stroke="#5CBC7D" stroke-linecap="round" x1="5" y1="12" x2="5" y2="1"></line>
+          <line className="icon-playing-bar3" stroke-width="1" stroke="#5CBC7D" stroke-linecap="round" x1="8" y1="12" x2="8" y2="1"></line>
+          <line className="icon-playing-bar4" stroke-width="1" stroke="#5CBC7D" stroke-linecap="round" x1="11" y1="12" x2="11" y2="1"></line>
+        </g>
+      </svg>
+    )
+  }
+
   render () {
     const { id, name, channelId } = this.props
     return (
       <li
-        className={ channelId === id ? 'channel active' : 'channel' }
-        key={ id }>
-        <span className='c-playing'></span>
+        className={ 'channel' + (channelId === id ? ' active' : '') } key={ id }>
+        { this.renderIcon() }
         <a href='javascript:;' onClick={ this.handleChannel.bind(this) }>{ name }</a>
       </li>
     )
